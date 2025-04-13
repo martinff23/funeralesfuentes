1f20fe01e6ca17e594516cb6d3b03c55e6a322db
Martin Fuentes Fernandez <martinfuentesfernandez9@gmail.com>
2025-04-12 18:35:38 -0600

    se resuelven conflictos de icono

--- views/adminLayout.php
+++ views/adminLayout.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FUNERALES FUENTES</title>
-    <link rel="icon" type="image/x-icon" href="/build/img/golden_ico.svg">
+    <link rel="icon" type="image/x-icon" href="/public/build/img/golden_ico.svg">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;700;900&display=swap" rel="stylesheet">
